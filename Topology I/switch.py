afa214b8848489c0bb51c7e1f699519a710ab751
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-05-31 12:23:42 -0300

    Fix get_switch_amount

--- Topology I/switch.py
+++ Topology I/switch.py
@@ -1,7 +1,8 @@
 import math
 
 def get_switch_amount(switch_level):
-	if switch_level = 1:
-		return 1
-	else:
-		return math.pow(2, switch_level - 1) + 2 * get_switch_amount(switch_level - 1)
+	sum = math.pow(2, switch_level - 1)
+	while (0 < switch_level - 1):
+		sum += math.pow(2, switch_level - 1)
+		switch_level -= 1
+	return sum
